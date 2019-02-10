a0a4d8e8eb8e2763b047745723ac0a5fd9e9283b
SujithVelayudhan <sujithvelayudhanhere@gmail.com>
2019-02-10 11:41:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sujith/gps_lat_long_sample/GpsLocationActivity.java
@@ -0,0 +1,120 @@
+package com.example.sujith.gps_lat_long_sample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GpsLocationActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    TextView latTextView,longTextView;
+    Button getLocationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_location);
+
+        initialise();
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        getLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location)
+                    {
+                        double lat=location.getLatitude();
+                        double lon=location.getLongitude();
+
+                        latTextView.setText("Latitude :"+lat);
+                        longTextView.setText("Longitude :"+lon);
+
+                        //Toast.makeText(GpsLocationActivity.this, "Direction: "+lat+"\n"+lon, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+
+                    ActivityCompat.requestPermissions((Activity) getApplicationContext(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+                }
+                else
+                {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+                }
+
+            }
+        });
+
+
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    }
+
+    private void initialise()
+    {
+        latTextView=findViewById(R.id.latp);
+        longTextView=findViewById(R.id.longp);
+        getLocationButton=findViewById(R.id.loc_btnp);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+                    ==PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            }
+
+        }
+    }
+}
